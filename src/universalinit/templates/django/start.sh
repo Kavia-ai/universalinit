c03e842a4471f31fe5b49e49bae3f4df534668cd
mmaquina <mmaquina@gmail.com>
2025-05-06 14:36:58 -0300

    change every framework to run on 3000 port

--- src/universalinit/templates/django/start.sh
+++ src/universalinit/templates/django/start.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 
-. venv/bin/activate
-
-PORT=3000
-
-while lsof -iTCP:$PORT -sTCP:LISTEN -n -P >/dev/null
-do
-  echo "Port $PORT is in use. Trying $((PORT+1))..."
-  PORT=$((PORT+1))
-done
-
-echo "Found free port: $PORT"
+source venv/bin/activate
 
 python manage.py migrate 
-python manage.py runserver 0.0.0.0:$PORT
+python manage.py runserver 0.0.0.0:3000
