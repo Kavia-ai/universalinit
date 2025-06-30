a348c24288f4b998743f2e3ad0397a922a4393f0
mmaquina <mmaquina@gmail.com>
2025-06-30 18:45:56 -0300

    enable endpoints in express and flask swagger interfaces

--- src/universalinit/templates/express/src/app.js
+++ src/universalinit/templates/express/src/app.js
@@ -12,13 +12,26 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
-
+app.set('trust proxy', true);
 app.use('/docs', swaggerUi.serve, (req, res, next) => {
+  const host = req.get('host');           // may or may not include port
+  let protocol = req.protocol;          // http or https
+
+  const actualPort = req.socket.localPort;
+  const hasPort = host.includes(':');
+  
+  const needsPort =
+    !hasPort &&
+    ((protocol === 'http' && actualPort !== 80) ||
+     (protocol === 'https' && actualPort !== 443));
+  const fullHost = needsPort ? `${host}:${actualPort}` : host;
+  protocol = req.secure ? 'https' : protocol;
+
   const dynamicSpec = {
     ...swaggerSpec,
     servers: [
       {
-        url: `${req.protocol}://${req.get('host')}`,
+        url: `${protocol}://${fullHost}`,
       },
     ],
   };
