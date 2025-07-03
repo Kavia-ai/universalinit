f1af54950808bc11f1e74984312fc9ad04b25e68
Ignacio Lescano <mmaquina@gmail.com>
2025-07-03 12:25:27 -0500

    Add configure_environment command to ProjectConfig class

--- src/universalinit/templateconfig.py
+++ src/universalinit/templateconfig.py
@@ -127,6 +127,12 @@ class EnvironmentConfig:
     android_sdk_version: str = ""
 
 @dataclass
+class ConfigureEnvironment:
+    """Run Environment configuration"""
+    command: str
+    working_directory: str
+    
+@dataclass
 class RunTool:
     """Run tool configuration."""
     command: str
@@ -141,6 +147,7 @@ class TestTool:
 @dataclass
 class TemplateInitInfo:
     """Complete template initialization information."""
+    configure_enviroment: ConfigureEnvironment
     build_cmd: BuildCommand
     install_dependencies: InstallDependenciesCommand
     env_config: EnvironmentConfig
@@ -173,6 +180,10 @@ class TemplateConfigProvider:
             config_data = yaml.safe_load(data)
 
         return TemplateInitInfo(
+            configure_enviroment=ConfigureEnvironment(
+                command=config_data['configure_environment']['command'],
+                working_directory=config_data['configure_environment']['working_directory']
+            ),
             build_cmd=BuildCommand(
                 command=config_data['build_cmd']['command'],
                 working_directory=config_data['build_cmd']['working_directory']
