c666346824326ff58e126898d7de78ee8c0be118
Zoltan Kuscsik <zoltan@s57.io>
2025-07-07 08:14:26 +0200

    Updated init

--- universalinit-env/src/universalinit_env/__init__.py
+++ universalinit-env/src/universalinit_env/__init__.py
@@ -4,19 +4,14 @@ UniversalInit Environment Package
 A package for mapping environment variables between different frameworks and a common format.
 """
 
-from .envmapper import (
-    get_template_path,
-    parse_template_file,
-    map_common_to_framework,
-    map_framework_to_common,
-    get_supported_frameworks,
-)
+from .envmapper import *
 
-__version__ = "0.1.0"
+__version__ = "0.1.2"
 __all__ = [
     "get_template_path",
     "parse_template_file", 
     "map_common_to_framework",
     "map_framework_to_common",
     "get_supported_frameworks",
+    "apply_wildcard_mapping",
 ]
