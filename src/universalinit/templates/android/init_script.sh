d0d3c9b7720903507baaab9962b34da0b9a5a1dd
gerardo-rios-kavia <gerardo@kavia.ai>
2025-08-06 17:36:56 -0500

    updating templates to work with the new environment

--- src/universalinit/templates/android/init_script.sh
+++ src/universalinit/templates/android/init_script.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-/opt/gradle/gradle-9.0.0/bin/gradle init -Dorg.gradle.buildinit.specs=org.gradle.experimental.android-ecosystem-init:0.1.44 \
-     --use-defaults   --overwrite
+/opt/gradle/gradle-9.0.0/bin/gradle init \
+  -Dorg.gradle.buildinit.specs=org.gradle.experimental.android-ecosystem-init:0.1.44 \
+  -Dorg.gradle.buildinit.language=java \
+  --use-defaults \
+  --overwrite
