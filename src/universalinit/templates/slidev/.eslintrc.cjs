72c1bdd8cb65d2e4d6afc03244fd931b1deef7c9
@raphael-kavia <raphael@kavia.ai>
2025-09-01 16:01:17 -0300

    Simplifying lint script and fixing command for slidev

--- src/universalinit/templates/slidev/.eslintrc.cjs
+++ src/universalinit/templates/slidev/.eslintrc.cjs
@@ -5,38 +5,17 @@ module.exports = {
     node: true,
     es2021: true,
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:vue/vue3-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
   parser: 'vue-eslint-parser',
   parserOptions: {
-    ecmaVersion: 2021,
-    parser: '@typescript-eslint/parser',
+    ecmaVersion: 'latest',
     sourceType: 'module',
+    parser: '@typescript-eslint/parser',
   },
-  plugins: ['vue', '@typescript-eslint'],
-  rules: {
-    'vue/multi-word-component-names': 'off',
-    'vue/require-default-prop': 'off',
-    'vue/no-v-html': 'off',
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    '@typescript-eslint/no-explicit-any': 'warn',
-    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-  },
+  plugins: [],
+  rules: {},
   ignorePatterns: [
     'dist',
     'node_modules',
     '*.d.ts',
-  ],
-  overrides: [
-    {
-      files: ['*.md'],
-      parser: 'markdown-eslint-parser',
-      extends: ['plugin:markdown/recommended'],
-    },
   ],
 };
